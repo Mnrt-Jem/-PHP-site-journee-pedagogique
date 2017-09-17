c400335dbf4f7cac36341a0b90f408803b64610e
tomsmartin <thomas.martin60700@gmail.com>
2017-09-17 10:37:29 +0200

    add footer
    Utiliser l'include maintenant pour le footer

---
+++ footer.php
@@ -0,0 +1,33 @@
+<!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item">
+                
+                  <span class="fa-stack fa-lg">
+                    <img src="img/logo.png">
+                  </span>
+                  <br>
+                  <p>Lycée Saint-Vincent SUAVITER & FORTITER</p>
+                
+              </li>
+            </ul>
+            <p class="copyright text-muted">Copyright &copy; Lycée Saint-Vincent</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/clean-blog.min.js"></script>
+
+  </body>
+
+</html>
