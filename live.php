2ed5b36051fa10408869823f147939fa8696da63
tomsmartin <thomas.martin60700@gmail.com>
2017-09-17 10:39:03 +0200

    add include footer

---
+++ live.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Clean Blog - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/clean-blog.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="post.html">Sample Post</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>Clean Blog</h1>
+              <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-9">
+            <div class="col-lg-3 col-lg-offset-1" style="display: inline-block;">
+            <iframe width="175" height="150" src="https://www.youtube.com/embed/JmQJVP6LBTQ" frameborder="0" allowfullscreen></iframe>
+            <p class="text-muted">DOFUS ENTRE DANS L'ESPORT 1</p>
+            </div>
+            <div class="col-lg-3" style="display: inline-block;">
+              <iframe width="175" height="150" src="https://www.youtube.com/embed/JmQJVP6LBTQ" frameborder="0" allowfullscreen></iframe>
+              <p>DOFUS ENTRE DANS L'ESPORT 2</p>
+            </div>
+            <div class="col-lg-3" style="display: inline-block;">
+              <iframe width="175" height="150" src="https://www.youtube.com/embed/JmQJVP6LBTQ" frameborder="0" allowfullscreen></iframe>
+              <p>DOFUS ENTRE DANS L'ESPORT 3</p>
+            </div>
+            <div class="col-lg-3" style="display: inline-block;">
+            <iframe width="175" height="150" src="https://www.youtube.com/embed/JmQJVP6LBTQ" frameborder="0" allowfullscreen></iframe>
+              <p>DOFUS ENTRE DANS L'ESPORT 4</p>
+            </div>
+             <div class="col-lg-3" style="display: inline-block;">
+             <iframe width="175" height="150" src="https://www.youtube.com/embed/JmQJVP6LBTQ" frameborder="0" allowfullscreen></iframe>
+              <p>DOFUS ENTRE DANS L'ESPORT 5</p>
+            </div>
+            <div class="col-lg-3" style="display: inline-block;">
+            <iframe width="175" height="150" src="https://www.youtube.com/embed/JmQJVP6LBTQ" frameborder="0" allowfullscreen></iframe>
+              <p>DOFUS ENTRE DANS L'ESPORT 6</p>
+            </div>
+        </div>
+        <div class="col-lg-3" style="float: right;">
+                    <a class="twitter-timeline"  href="https://twitter.com/hashtag/SaintVincent" data-widget-id="907288860698320896">Tweets sur #SaintVincent</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
+        </div>
+    </div>
+    </div>
+
+    <hr>
+
+    <?php include 'footer.php'; ?>
