fb88ade5e3140630ce875ceeb286935243ffe632
tomsmartin <thomas.martin60700@gmail.com>
2017-10-16 14:20:42 +0200

    Update retour.php

--- retour.php
+++ retour.php
@@ -1,84 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+  include 'header.php';
+?>
 
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Journée Pédagogique</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- Custom styles for this template -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="post.html">Sample Post</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <!-- Page Header -->
-    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="site-heading">
-              <h1>Clean Blog</h1>
-              <span class="subheading">A Blog Theme by Start Bootstrap</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
 
     <!-- Main Content -->
-    <div class="container">
+    <hr>
+    <div class="container" style="margin-left: 180px;">
       <div class="row">
-        <div class="col-lg3 col-md-3 col-sm-3 col-xs-3">
+        <div class="col-lg3 col-md-3 col-sm-3 col-xs-3" style="margin-right:-30px;">
           <img src="img/traitement.png" style="width: 150px; padding-bottom: 50px;">
           <img src="img/traitement.png" style="width: 150px;padding-bottom: 50px;">
           <img src="img/traitement.png" style="width: 150px;padding-bottom: 50px;">
           <img src="img/traitement.png" style="width: 150px;padding-bottom: 50px;">       
         </div>          
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-          <img src="img/fraction-vierge-f.jpeg" style="width: 380px; margin-left: 50px;  ">
-          <img src="img/traitement.png" style="width: 150px;">
-          <img src="img/traitement.png" style="width: 150px;">
+          <!-- <img src="img/fraction-vierge-f.jpeg" style="width: 380px; margin-left: 50px; margin-bottom: 210px;  "> -->
+          <p style="font-family: 'Source Sans Pro';">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <img src="img/traitement.png" style="width: 150px; margin-left: -30px; margin-right: 40px;">
+          <img src="img/traitement.png" style="width: 150px; margin-right: 40px;">
           <img src="img/traitement.png" style="width: 150px;">          
         </div>
         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
@@ -92,4 +34,5 @@
 
     <hr>
 
-        <?php include 'footer.php'; ?>
+<?php include 'footer.php'; ?>
+
