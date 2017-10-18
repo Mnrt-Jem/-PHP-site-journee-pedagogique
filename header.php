15fa616868423e50d09c90ad642053215d121483
Migiway <marc.iguenane@gmail.com>
2017-10-18 11:20:18 +0200

    header de swagg

--- header.php
+++ header.php
@@ -40,36 +40,40 @@
           </div>
         </div>
       </div>
-      <div class="container" style="background-color: white; padding-bottom: 20px;">
+
+
+      <div class="container">
         <div class="row">
-      	 <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel" style="width: 100%;" >
-              <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-              </ol>
-              <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                  <img class="d-block img-fluid" src="img/Accueil_header/Abbaye-St-Vincent_batiment-conventuel.jpg" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block img-fluid" src="img/Accueil_header/Abbaye-St-Vincent_Cloitre.jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block img-fluid" src="img/Accueil_header/Abbaye-St-Vincent_jardin-du-Cloitre.jpg" alt="Third slide">
-                </div>
+      	 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="img/Accueil_header/Abbaye-St-Vincent_Cloitre.jpg" alt="First slide">
               </div>
-              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="img/Accueil_header/Abbaye-St-Vincent_jardin-du-Cloitre.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="img/Accueil_header/Abbaye-St-Vincent_batiment-conventuel.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
           </div>
         </div>
       </div>
+
+
       <div class="container" id="menu_header">
         <div class="row">
           <div class="col-lg-3" id="header_menu_accueil"> 
