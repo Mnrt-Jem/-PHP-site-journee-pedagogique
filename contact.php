4a9f0ad6586e70b653fcbab68fa0b668408af131
Davidzer <david.degaugue@hotmail.com>
2017-10-18 11:19:01 +0200

    Add files via upload

--- contact.php
+++ contact.php
@@ -2,6 +2,8 @@
     include 'header.php';
  ?>
 
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
     <!-- Main Content -->
        <div class="container">
 
@@ -17,7 +19,7 @@
                     <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2606.835887247322!2d2.5862843203064156!3d49.20367274679404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e630cfeb73f31d%3A0x48c819ca44bf7503!2sLyc%C3%A9e+Priv%C3%A9+Saint+Vincent!5e0!3m2!1sfr!2sfr!4v1505133146617" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4-sm-12">
                     <p>Téléphone:
                         <strong>03.44.53.96.40</strong>
                     </p>
@@ -29,45 +31,64 @@
             </div>
         <br>
 
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12">
-                    <hr>
-                    <h2 class="intro-text text-center">Envoyer un message
-                        <strong></strong>
-                    </h2>
-                    <hr>
-                    <form role="form">
-                        <div class="row">
-                            <div class="form-group col-lg-4">
-                                <label>Nom</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group col-lg-4">
-                                <label>Adresse Email</label>
-                                <input type="email" class="form-control">
-                            </div>
-                            <div class="form-group col-lg-4">
-                                <label>Numéro de Téléphone</label>
-                                <input type="tel" class="form-control">
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="form-group col-lg-12">
-                                <label>Message</label>
-                                <textarea class="form-control" rows="6"></textarea>
-                            </div>
-                            <div class="form-group col-lg-12">
-                                <input type="hidden" name="save" value="contact">
-                                <button type="submit" class="btn btn-default">Envoyer</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-      </div>
+           <section id="contact">
+               <div class="container">
+                   <div class="row">
+                       <div class="col-lg-12 text-center">
+                           <h2 class="section-heading">Contact</h2>
+                       </div>
+                   </div>
+                   <div class="row">
+                       <div class="col-lg-12">
+                           <form id="contactForm" name="sentMessage" novalidate>
+                               <div class="row">
+                                   <div class="col-md-6">
+                                       <div class="form-group">
+                                           <input class="form-control" id="name" type="text" placeholder="Votre Nom *" required data-validation-required-message="Please enter your name.">
+                                           <p class="help-block text-danger"></p>
+                                       </div>
+                                       <div class="form-group">
+                                           <input class="form-control" id="email" type="email" placeholder="Votre Email *" required data-validation-required-message="Please enter your email address.">
+                                           <p class="help-block text-danger"></p>
+                                       </div>
+                                       <div class="form-group">
+                                           <input class="form-control" id="phone" type="tel" placeholder="Votre téléphone *" required data-validation-required-message="Please enter your phone number.">
+                                           <p class="help-block text-danger"></p>
+                                       </div>
+                                   </div>
+                                   <div class="col-md-6">
+                                       <div class="form-group">
+                                           <textarea class="form-control" id="message" placeholder="Votre message *" required data-validation-required-message="Please enter a message."></textarea>
+                                           <p class="help-block text-danger"></p>
+                                       </div>
+                                   </div>
+                                   <div class="clearfix"></div>
+                                   <div class="col-lg-12 text-center">
+                                       <div id="success"></div>
+                                       <button id="sendMessageButton" class="btn btn-xl" type="submit">Envoyer</button>
+                                   </div>
+                               </div>
+                           </form>
+                       </div>
+                   </div>
+               </div>
+           </section>
+
+           <!-- Bootstrap core JavaScript -->
+           <script src="vendor/jquery/jquery.min.js"></script>
+           <script src="vendor/popper/popper.min.js"></script>
+           <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+           <!-- Plugin JavaScript -->
+           <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+           <!-- Contact form JavaScript -->
+           <script src="js/jqBootstrapValidation.js"></script>
+           <script src="js/contact_me.js"></script>
 
 
-    <hr>
+
+
+           <hr>
 
     <?php include 'footer.php'; ?>
